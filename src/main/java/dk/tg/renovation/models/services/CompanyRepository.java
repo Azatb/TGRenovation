21288206a35680311b03e40f85b8a023dd5ad798
Unknown <koen_jolle@hotmail.com>
2017-11-23 18:42:28 +0100

    Nu er jeg færdig med at koge, im sowwie

--- src/main/java/dk/tg/renovation/models/services/CompanyRepository.java
+++ src/main/java/dk/tg/renovation/models/services/CompanyRepository.java
@@ -63,8 +63,7 @@ public class CompanyRepository implements ICrud<Company>, Ilogin<Company> {
 
     @Override
     public void update(Company company) {
-        System.out.println("LuL");
-        System.out.println("Mega LUL");
+        
     }
 
     @Override
